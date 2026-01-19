93cbb3a736bb8a608310e3dba95f59ee6d6115aa
x3alone <otmanchouarix3@gmail.com>
2026-01-19 12:31:38 +0100

    la danse berlioz

--- head-and-tail.sh
+++ head-and-tail.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl -s "https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt" | \
-{ head -n1 | sed 's/.*/Hello My username is: &,/'; tail -n1 | sed 's/.*/so my passwd is: &/'; }
+(head -n1 && tail -n1)
