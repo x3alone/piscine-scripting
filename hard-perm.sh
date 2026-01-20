a81828d206eb12de3d3008cac46faf2508ac5b66
x3alone <otmanchouarix3@gmail.com>
2026-01-20 11:41:33 +0100

    metalmind kinji

--- hard-perm.sh
+++ hard-perm.sh
@@ -2,14 +2,14 @@
 
 cd hard-perm || exit 1
 
-chmod 701 0
-chmod 777 3
-chmod 701 A
-chmod 602 1
-chmod 604 2
-chmod 510 4
-chmod 260 5
-chmod 260 6
-chmod 510 7
-chmod 604 8
-chmod 602 9
+chmod 701 0     
+chmod 777 3     
+chmod 701 A     
+chmod 602 1     
+chmod 604 2     
+chmod 510 4     
+chmod 340 5     
+chmod 340 6     
+chmod 510 7     
+chmod 604 8     
+chmod 602 9 
