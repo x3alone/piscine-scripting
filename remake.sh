d79ed5e57684d954501cc14fbfc920509692f8b7
x3alone <otmanchouarix3@gmail.com>
2026-01-22 11:49:35 +0100

    El Dicharachero antonio aguilar

--- remake.sh
+++ remake.sh
@@ -7,21 +7,22 @@ fi
 
 TARGET="$1"
 
-touch "$TARGET/ciao"
-chmod u=r,g=r,o=w "$TARGET/ciao"
-touch -t 202601010001 "$TARGET/ciao"
-mkdir "$TARGET/mamma"
-chmod u=rwx,g=rwx,o=rwx "$TARGET/mamma"
-touch -t 202601020001 "$TARGET/mamma"
-touch "$TARGET/guarda"
-chmod u=r,g=,o= "$TARGET/guarda"
-touch -t 202601030001 "$TARGET/guarda"
-touch "$TARGET/come"
-chmod u=rw,g=r,o=w "$TARGET/come"
-touch -t 202601040001 "$TARGET/come"
-mkdir "$TARGET/mi"
-chmod u=r,g=x,o=w "$TARGET/mi"
-touch -t 202601050001 "$TARGET/mi"
-touch "$TARGET/diverto"
-chmod u=r,g=w,o=x "$TARGET/diverto"
-touch -t 202601060001 "$TARGET/diverto"
+touch -t 202201010001 "$TARGET/ciao"
+chmod 444 "$TARGET/ciao"
+
+mkdir -p "$TARGET/mamma"
+chmod 777 "$TARGET/mamma"
+touch -t 202201020001 "$TARGET/mamma"
+
+touch -t 202201030001 "$TARGET/guarda"
+chmod 400 "$TARGET/guarda"
+
+touch -t 202201040001 "$TARGET/come"
+chmod 624 "$TARGET/come"
+
+mkdir -p "$TARGET/mi"
+chmod 205 "$TARGET/mi"
+touch -t 202201050001 "$TARGET/mi"
+
+touch -t 202201060001 "$TARGET/diverto"
+chmod 471 "$TARGET/diverto"
