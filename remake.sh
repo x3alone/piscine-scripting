f04ff63ef2ce02f12ec04e5137becda706a63870
x3alone <otmanchouarix3@gmail.com>
2026-01-22 11:22:08 +0100

    el cantador

---
+++ remake.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Error"
+    exit 1
+fi
+
+TARGET_DIR="$1"
+
+if [[ ! -d "$TARGET_DIR" ]]; then
+    echo "Error"
+    exit 1
+fi
+
+touch -t 202201010001 "$TARGET_DIR/ciao"
+chmod 444 "$TARGET_DIR/ciao"
+mkdir -p "$TARGET_DIR/mamma"
+chmod 777 "$TARGET_DIR/mamma"
+touch -t 202201020001 "$TARGET_DIR/mamma"
+touch -t 202201030001 "$TARGET_DIR/guarda"
+chmod 400 "$TARGET_DIR/guarda"
+touch -t 202201040001 "$TARGET_DIR/come"
+chmod 644 "$TARGET_DIR/come"
+mkdir -p "$TARGET_DIR/mi"
+chmod 205 "$TARGET_DIR/mi"
+touch -t 202201050001 "$TARGET_DIR/mi"
+touch -t 202201060001 "$TARGET_DIR/diverto"
+chmod 071 "$TARGET_DIR/diverto"
