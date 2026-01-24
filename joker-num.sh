ad834fbe0aea9146f51cc4a5295172a5861d5178
x3alone <otmanchouarix3@gmail.com>
2026-01-24 14:07:43 +0100

    don't be dumb

--- joker-num.sh
+++ joker-num.sh
@@ -3,31 +3,30 @@
 secret="$1"
 
 if [ "$#" -ne 1 ] || ! [[ "$secret" =~ ^[0-9]+$ ]] || [ "$secret" -lt 1 ] || [ "$secret" -gt 100 ]; then
-    printf "Error: wrong argument"
+    echo "Error: wrong argument"
     exit 1
 fi
 
 tries=5
-valid_guesses=0
+used=0
 
-for ((i=1; i<=tries; )); do
-    tries_left=$((tries - valid_guesses))
+for ((; used < tries; )); do
+    tries_left=$((tries - used))
     echo "Enter your guess ($tries_left tries left):"
     read guess
 
-    if ! [[ "$guess" =~ ^[0-9]+$ ]]; then
+    if [[ -z "$guess" ]] || ! [[ "$guess" =~ ^[0-9]+$ ]] || [ "$guess" -lt 1 ] || [ "$guess" -gt 100 ]; then
         continue
     fi
 
-    valid_guesses=$((valid_guesses + 1))
-    i=$((i + 1))
+    used=$((used + 1))
 
     if [ "$guess" -gt "$secret" ]; then
         echo "Go down"
     elif [ "$guess" -lt "$secret" ]; then
         echo "Go up"
     else
-        echo "Congratulations, you found the number in $valid_guesses moves!"
+        echo "Congratulations, you found the number in $used moves!"
         exit 0
     fi
 done
