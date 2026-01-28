8d2b70eed81d058fd2835a25dc06f1c276070a35
root <root@DESKTOP-KQ3TGFJ.localdomain>
2026-01-28 16:22:52 +0100

    little lie ODIE

---
+++ custom_calendar.py
@@ -0,0 +1,24 @@
+def day_from_number(day_number):
+    days = {
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday"
+    }
+    return days.get(day_number)
+
+
+def day_to_number(day):
+    days = {
+        "Monday": 1,
+        "Tuesday": 2,
+        "Wednesday": 3,
+        "Thursday": 4,
+        "Friday": 5,
+        "Saturday": 6,
+        "Sunday": 7
+    }
+    return days.get(day)
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-import clean_the_list
-shopping_list = ['tomatoes', 'pastas', 'milk', 'salt']
-print(clean_the_list.clean_list(shopping_list))
+import custom_calendar
+
+print(custom_calendar.day_from_number(2))
+print(custom_calendar.day_from_number(1))
+print(custom_calendar.day_from_number(1000))
+print(custom_calendar.day_to_number('Sunday'))
+print(custom_calendar.day_to_number('invalid day'))
